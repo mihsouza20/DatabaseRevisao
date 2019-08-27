02fc1edc2225f17c10d1f7c26f12e82d892ee5b0
mihsouza2906 <mih_jovemaprendiz@hotmail.com>
2019-08-26 23:27:24 -0300

    Revisao atualizada UNIQUE

--- DatabaseRevisao.sql
+++ DatabaseRevisao.sql
@@ -16,7 +16,7 @@ GO
 
 
 
-create table tb_vendas(
+//*create table tb_vendas(
        id_venda int primary key IDENTITY(1,1),
        id_cliente int not null,
 	   id_item_vendido int not null,
@@ -78,8 +78,82 @@ alter table tb_item_vendido
 	  alter table tb_clientes
       ADD CONSTRAINT fk_id_cliente
       FOREIGN KEY (id_cliente) REFERENCES tb_clientes(id_cliente)
-	  GO
+	  GO *//
 
-	  
+CREATE TABLE produtos (
+   idProduto INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+   descricao NVARCHAR(50) NOT NULL,
+   estoqueAtual INT NOT NULL DEFAULT 0,
+   estoqueMin INT,
+   precoVenda DECIMAL(9,2) NOT NULL
+);
+
+GO
+
+CREATE TABLE clientes (
+   idCliente INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+   nomeCliente NVARCHAR(50) NOT NULL
+);
+
+GO
+
+CREATE TABLE vendas (
+   idVenda INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+   idCliente INT NOT NULL,
+   data DATE NOT NULL,
+   desconto DECIMAL (4,2)
+);
+
+GO
+
+CREATE TABLE itemVenda (
+   idItVenda INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+   idVenda INT NOT NULL,
+   idProduto INT NOT NULL,
+   qtdItVenda INT NOT NULL
+);
+
+GO
+
+CREATE TABLE vendaCancelada (
+   idVendaCancelada INT NOT NULL UNIQUE
+);
+
+ALTER TABLE vendas ADD CONSTRAINT fk_venda_cli FOREIGN KEY (idCliente) REFERENCES clientes(idCliente);
+ALTER TABLE itemVenda ADD CONSTRAINT fk_itemvend_venda FOREIGN KEY (idVenda) REFERENCES vendas(idVenda);
+ALTER TABLE itemVenda ADD CONSTRAINT fk_itemvend_prod FOREIGN KEY (idProduto) REFERENCES produtos(idProduto);
+ALTER TABLE vendaCancelada ADD CONSTRAINT fk_vendacanc_venda FOREIGN KEY (idVendaCancelada) REFERENCES vendas(idVenda);
+
+INSERT INTO produtos (descricao, estoqueAtual, precoVenda) VALUES ('Item 1', 100, 5.00);
+INSERT INTO produtos (descricao, estoqueAtual, precoVenda) VALUES ('Item 2', 100, 10.00);
+INSERT INTO produtos (descricao, estoqueAtual, precoVenda) VALUES ('Item 3', 100, 15.00);
+INSERT INTO produtos (descricao, estoqueAtual, precoVenda) VALUES ('Item 4', 100, 20.00);
+INSERT INTO produtos (descricao, estoqueAtual, precoVenda) VALUES ('Item 5', 100, 25.00);
+
+INSERT INTO clientes (nomeCliente) VALUES ('José');
+INSERT INTO clientes (nomeCliente) VALUES ('Maria');
+INSERT INTO clientes (nomeCliente) VALUES ('João');
+INSERT INTO clientes (nomeCliente) VALUES ('Ana');
+INSERT INTO clientes (nomeCliente) VALUES ('Luiz');
+
+INSERT INTO vendas (idCliente, data, desconto) VALUES (1, '2019-08-16', 10);
+INSERT INTO vendas (idCliente, data, desconto) VALUES (2, '2019-08-16', 5);
+INSERT INTO vendas (idCliente, data, desconto) VALUES (3, '2019-08-16', 8);
+INSERT INTO vendas (idCliente, data, desconto) VALUES (4, '2019-08-16', 3);
+INSERT INTO vendas (idCliente, data, desconto) VALUES (5, '2019-08-16', 7);
+
+INSERT INTO itemVenda (idVenda, idProduto, qtdItVenda) VALUES (1, 5, 1);
+INSERT INTO itemVenda (idVenda, idProduto, qtdItVenda) VALUES (2, 4, 3);
+INSERT INTO itemVenda (idVenda, idProduto, qtdItVenda) VALUES (3, 3, 5);
+INSERT INTO itemVenda (idVenda, idProduto, qtdItVenda) VALUES (4, 2, 7);
+INSERT INTO itemVenda (idVenda, idProduto, qtdItVenda) VALUES (5, 1, 5);
+INSERT INTO itemVenda (idVenda, idProduto, qtdItVenda) VALUES (5, 5, 3);
+
+INSERT INTO vendaCancelada (idVendaCancelada) VALUES (3);
+
+
+
+
+
 
 -- Professor não oconsegui fazer sozinha 
